0f8d8965ee8338da08cc134219751f8e05ed4b55
Chandan <chandanisro@gmail.com>
2025-02-04 18:07:26 +0530

    shop/home update

--- my_ecom/urls.py
+++ my_ecom/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('', include('my_shop.urls')),
+    path('shop/', include('my_shop.urls')),
     path('order/', include('my_order.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my_shop/urls.py
+++ my_shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
 
 ]
