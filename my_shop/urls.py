2ac6976c6825973782c2894d6a20d714d9c99b84
Chandan <chandanisro@gmail.com>
2025-02-06 19:42:10 +0530

    all functionality working properly

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/', views.product, name='product'),
+    path('product/', views.product, name='all_product'),
     path('product/<int:category_id>/', views.product, name='category_product'),
     path('product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
 
